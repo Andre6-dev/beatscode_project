491519fe7532c09703419b9419c03a7b470a271d
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-06-26 17:14:15 -0500

    Showing Posts in Search Screen
    Using flutter_staggered_grid_view to show the post images in a different way like a grid but with different sizes.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:beatscode_project/utils/colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -65,7 +66,28 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             )
-          : Text('Posts'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  );
+                }
+
+                /*CUSTOM GRID TO VIEW POSTS*/
+                return MasonryGridView.count(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  itemBuilder: (context, index) => Image.network(
+                    (snapshot.data! as dynamic).docs[index]['postUrl'],
+                  ),
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                );
+              }),
     );
   }
 }
